f03668785252e6f5c5f1a5c6fd885b948903167c
go-tiger <dev.gotiger@gmail.com>
2026-01-03 00:38:43 +0900

    feat: 서버 종료 시 봇 즉시 오프라인 처리

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app.module.js';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createApplicationContext(AppModule);
+
+  app.enableShutdownHooks();
+
+  process.on('SIGINT', async () => {
+    await app.close();
+    process.exit(0);
+  });
+
+  process.on('SIGTERM', async () => {
+    await app.close();
+    process.exit(0);
+  });
 }
 bootstrap();
