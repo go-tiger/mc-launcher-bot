d3dc447a20489b537563d06906d1c5608199bd01
go-tiger <dev.gotiger@gmail.com>
2026-01-03 00:14:49 +0900

    feat: 환경변수 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { NecordModule } from 'necord';
+import { IntentsBitField } from 'discord.js';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    NecordModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        token: configService.get<string>('DISCORD_TOKEN'),
+        intents: [
+          IntentsBitField.Flags.Guilds,
+          IntentsBitField.Flags.GuildMessages,
+          IntentsBitField.Flags.GuildMembers,
+        ],
+      }),
+    }),
+  ],
 })
 export class AppModule {}
