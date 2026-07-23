593b94e406959550cbe3e0edf9e88744079cdf40
go-tiger <dev.gotiger@gmail.com>
2026-07-23 10:17:32 +0900

    feat: TicketLauncher 서브테이블 추가
    - 런처 의뢰 전용 스펙(launcherName, folderName, minecraftVersion,
      modLoader, loaderVersion, deadline)을 Ticket에서 분리
    - ticket을 PK 겸 FK로 사용해 1:1 관계를 구조적으로 보장

--- src/core/entities/index.ts
+++ src/core/entities/index.ts
@@ -2,3 +2,7 @@ export * from './commission.entity.js';
 export * from './guild-config.entity.js';
 export * from './guild-settings.entity.js';
 export * from './ticket.entity.js';
+export {
+  TicketLauncher,
+  ModLoader as TicketLauncherModLoader,
+} from './ticket-launcher.entity.js';
---
+++ src/core/entities/ticket-launcher.entity.ts
@@ -0,0 +1,42 @@
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Ticket } from './ticket.entity.js';
+
+export enum ModLoader {
+  FORGE = 'FORGE',
+  FABRIC = 'FABRIC',
+  NEOFORGE = 'NEOFORGE',
+}
+
+@Entity('ticket_launchers')
+export class TicketLauncher {
+  @PrimaryColumn()
+  id: number;
+
+  @OneToOne(() => Ticket)
+  @JoinColumn({ name: 'id' })
+  ticket: Ticket;
+
+  @Column()
+  launcherName: string;
+
+  @Column()
+  folderName: string;
+
+  @Column()
+  minecraftVersion: string;
+
+  @Column({ type: 'enum', enum: ModLoader })
+  modLoader: ModLoader;
+
+  @Column()
+  loaderVersion: string;
+
+  @Column()
+  deadline: Date;
+}
